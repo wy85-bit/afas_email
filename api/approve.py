23d56dbd3ed6aebb75f44c989b80acb149713cc7
wy85-bit <149866791+wy85-bit@users.noreply.github.com>
2026-03-17 16:13:44 +0100

    Update approve.py

--- api/approve.py
+++ api/approve.py
@@ -16,7 +16,7 @@ class handler(BaseHTTPRequestHandler):
         }
 
         # Targeting Monday, March 16th
-        test_date = "2026-03-17" 
+        test_date = "2026-03-16" 
         
         payload = {
             "PtRealization": {  # <--- Changed this to match the endpoint name
